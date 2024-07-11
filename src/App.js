37d8fd3719af19d15e7f7e166c952b4904ecd87b
Néstor <nestorblancotech@gmail.com>
2024-07-11 14:57:26 +0200

    Started the Main Page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolioo :P</h1>
+    <div className = "App">
+      <div className = "mainText">
+        <h1>Néstor Blanco</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo lobortis, tempus enim et, tristique est. Proin sed ipsum eu felis varius finibus. Quisque rhoncus, lorem sed tempor semper, massa diam rhoncus est, nec molestie ligula massa sed ex. Vivamus hendrerit, mi vel vulputate facilisis, felis enim vehicula enim, id dictum nibh urna eget erat.</p>
+      </div>
     </div>
   );
-}
+} 
 
 export default App;
