89b0157f837c5d9449238a868cd8a834a83b3fe2
Néstor <nestorblancotech@gmail.com>
2024-07-12 13:00:22 +0200

    Testing mobile ports

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
         <h1>Néstor Blanco</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo lobortis, tempus enim et, tristique est. Proin sed ipsum eu felis varius finibus. Quisque rhoncus, lorem sed tempor semper, massa diam rhoncus est, nec molestie ligula massa sed ex. Vivamus hendrerit, mi vel vulputate facilisis, felis enim vehicula enim, id dictum nibh urna eget erat.</p>
       </div>
+      <p className="testting">You're on a phone</p>
     </div>
   );
 } 
